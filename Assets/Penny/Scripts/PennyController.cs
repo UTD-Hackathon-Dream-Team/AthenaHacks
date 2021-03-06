6991209f2e602942ff527044f438cbe17cf35001
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-03-06 14:50:24 -0600

    add main character

---
+++ Assets/Penny/Scripts/PennyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PennyController : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        transform.position = position;
+
+        if (horizontal > 0.01f)
+        {
+            if (spriteRenderer.flipX == true)
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+        else if (horizontal < -0.01f)
+        {
+            if (spriteRenderer.flipX == false)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+
+        animator.SetBool("grounded", true);
+        animator.SetFloat("velocityX", Mathf.Abs(horizontal));
+    }
+}
