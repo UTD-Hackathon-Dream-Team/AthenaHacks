9a3604a6156035988fb1f895c9321d6b71b427cf
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-03-06 23:25:32 -0600

    start adding sign

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError("bitch");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && playerInRange)
+        {
+            if (dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogText.text = dialog;
+                dialogBox.SetActive(true);
+            }
+        }
+    }
+
+    private void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.LogError("in range");
+            playerInRange = true;
+        }
+    }
+
+    private void onTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+            Debug.LogError("yeet");
+        }
+    }
+}
